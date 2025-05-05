8e509ad9dbb9c7499681218c4b19e703992cf7b6
ian balijawa <ianbalijawa16@gmail.com>
2025-05-05 17:34:11 +0300

    updates

--- src/entities/gallery-item.entity.ts
+++ src/entities/gallery-item.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
-export type GalleryCategory = 'events' | 'facilities' | 'training' | 'competitions';
+export type GalleryCategory = 'events' | 'facilities' | 'training' | 'competitions' | 'stays' | 'activity' | 'videos';
 export type GalleryType = 'image' | 'video' | 'both';
 
 @Entity( 'gallery_items' )
@@ -11,7 +11,7 @@ export class GalleryItem extends BaseEntity {
 
     @Column( {
         type: 'enum',
-        enum: ['events', 'facilities', 'training', 'competitions']
+        enum: ['events', 'facilities', 'training', 'competitions', 'stays', 'activity', 'videos']
     } )
     category: GalleryCategory;
 
